2d36139565f02ccf17a646480d63f2d0ac40c8f7
hannescato <ditt@example.com>
2025-04-07 12:19:07 +0200

    steg1 seems to be working correctly

--- start-code(1)/start-code/parser.c
+++ start-code(1)/start-code/parser.c
@@ -14,7 +14,7 @@
 /**********************************************************************/
 /* #include "keytoktab.h"   */       /* when the keytoktab is added   */
 /* #include "lexer.h"       */       /* when the lexer     is added   */
-/* #include "symtab.h"      */       /* when the symtab    is added   */
+/* #include "symtab.h"       */       /* when the symtab    is added   */
 /* #include "optab.h"       */       /* when the optab     is added   */
 
 /**********************************************************************/
@@ -24,18 +24,36 @@
 static int  lookahead=0;
 static int  is_parse_ok=1;
 
+// Funktionsprototyper
+static void prog();
+static void program_header();
+static void var_part();
+static void var_dec_list();
+static void var_dec();
+static void id_list();
+static void type();
+static void stat_part();
+static void stat_list();
+static void stat();
+static void assign_stat();
+static void expr();
+static void term();
+static void factor();
+static void operand();
+
 /**********************************************************************/
 /* RAPID PROTOTYPING - simulate the token stream & lexer (get_token)  */
 /**********************************************************************/
 /* define tokens + keywords NB: remove this when keytoktab.h is added */
 /**********************************************************************/
 enum tvalues { program = 257, id, input, output, var, integer, real, boolean, begin_, end_, number, assign};
+
 /**********************************************************************/
 /* Simulate the token stream for a given program                      */
 /**********************************************************************/
-static int tokens[] = {program, id, '(', input, ',', output, ')', ';','$',
+static int tokens[] = {program, id, '(', input, ',', output, ')', ';',
                         var, id, ',', id, ',', id, ':', integer, ';',
-                        begin_, id, assign, id, '+', id, '*', number, end_, '.'};
+                        begin_, id, assign, id, '+', id, '*', number, end_, '.', '$'};
 
 /**********************************************************************/
 /*  Simulate the lexer -- get the next token from the buffer          */
@@ -57,18 +75,17 @@ static void out(char* s)
 {
     if(DEBUG) printf("\n *** Out %s", s);
 }
+
 /**********************************************************************/
 /* The Parser functions                                               */
 /**********************************************************************/
 static void match(int t)
 {
-    if(DEBUG) printf("\n --------In match expected: %4d, found: %4d",
-                    t, lookahead);
+    if(DEBUG) printf("\n --------In match expected: %4d, found: %4d", t, lookahead);
     if (lookahead == t) lookahead = pget_token();
     else {
-    is_parse_ok=0;
-    printf("\n *** Unexpected Token: expected: %4d found: %4d (in match)",
-              t, lookahead);
+        is_parse_ok=0;
+        printf("\n *** Unexpected Token: expected: %4d found: %4d (in match)", t, lookahead);
     }
 }
 
@@ -76,53 +93,53 @@ static void match(int t)
 /* The grammar functions                                              */
 /**********************************************************************/
 static void prog(){
-in("prog");
-program_header();
-var_part();
-stat_part();
-out("prog");
+    in("prog");
+    program_header();
+    var_part();
+    stat_part();
+    out("prog");
 }
-static void prog_header(){
-    in("prog_header");
-    
 
-    out("prog_header");
+static void program_header(){
+    in("program_header");
+    match(program); match(id); match('('); match(input);
+    match(','); match(output); match(')'); match(';');
+    out("program_header");
 }
+
 static void var_part(){
-   in("var_part");
-   if(lookahead == var){
-    match(var);
-    var_dec_list();
-   }
-   out("var_part");
+    in("var_part");
+    if(lookahead == var){
+        match(var);
+        var_dec_list();
+    }
+    out("var_part");
 }
+
 static void var_dec_list(){
     in("var_dec_list");
     var_dec();
     if(lookahead == id){
-        var_dec();
+        var_dec_list();
     }
     out("var_dec_list");
 }
+
 static void var_dec(){
     in("var_dec");
     id_list();
     match(':');
-    if(lookahead == integer){
-        match(integer);
-    }else if(lookahead == real){
-        match(real);
-    }
+    type();
     match(';');
     out("var_dec");    
-
 }
+
 static void id_list(){
     in("id_list");
     match(id);
-    if(lookahead == ';'){
+    if(lookahead == ','){
         match(',');
-        match(id);
+        id_list();
     }
     out("id_list");
 }
@@ -133,7 +150,7 @@ static void type(){
         match(integer);
     }else if(lookahead == real){
         match(real);
-    }else if(lookahead == boleean){
+    }else if(lookahead == boolean){
         match(boolean);
     }
     out("type");
@@ -146,48 +163,52 @@ static void stat_part(){
     match(end_); 
     match('.');
     out("stat_part");
-
 }
+
 static void stat_list(){
     in("stat_list");
-        stat();
-        if(lookahead == ';'){
-            match(';');
-            stat();
-        }
-    
-    out("stat_list");
+    stat();
+    if(lookahead == ';'){
+        match(';');
+        stat_list();
     }
-static void stat(){
-in("stat");
-assign_stat();
+    out("stat_list");
 }
+
+static void stat(){
+    in("stat");
+    assign_stat();
+    out("stat");
+}
+
 static void assign_stat(){
     in("assign_stat");
     match(id);
     match(assign);
     expr();
     out("assign_stat");
-
 }
+
 static void expr(){
     in("expr");
     term();
     if(lookahead == '+'){
         match('+');
-        term();
+        expr();
     }
     out("expr");
 }
+
 static void term(){
     in("term");
     factor();
     if(lookahead == '*'){
         match('*');
-        factor();
+        term();
     }
     out("term");
 }
+
 static void factor(){
     in("factor");
     if(lookahead == '('){
@@ -199,34 +220,25 @@ static void factor(){
     }
     out("factor");
 }
+
 static void operand(){
     in("operand");
     if(lookahead == id){
         match(id);
-        
     }else if(lookahead == number){
         match(number);
     }
     out("operand");
 }
 
-static void program_header()
-{
-    in("program_header");
-    match(program); match(id); match('('); match(input);
-    match(','); match(output); match(')'); match(';');
-    out("program_header");
-}
-
 /**********************************************************************/
 /*  PUBLIC METHODS for this OBJECT  (EXPORTED)                        */
 /**********************************************************************/
-
 int parser()
 {
     in("parser");
     lookahead = pget_token();       // get the first token
-    program_header();               // call the first grammar rule
+    prog();                         // start parsing from top-level rule
     out("parser");
     return is_parse_ok;             // status indicator
 }
